476133c05f548230b7707cae381adfbc7910cea1
Aevan Candelaria <aevan.candelaria@gmail.com>
2020-02-04 17:16:14 +0800

    First commit

---
+++ Algorithms/src/Base26.java
@@ -0,0 +1,65 @@
+
+import java.util.ArrayList;
+
+
+public class Base26 {
+    private final ArrayList<Integer> digit;
+    private int length;
+    
+    public Base26(){
+        digit = new ArrayList<>();
+        digit.add(1);
+        length = 1;
+        
+    }
+    
+    public void increment(){
+        digit.set(0, digit.get(0) + 1);
+        
+        for(int i=0;i<length;i++){
+            if(digit.get(i) == 26){
+                digit.set(i, 0);
+                if(i == length - 1){
+                    digit.add(0);
+                    length++;
+                    break;
+                }
+                else{
+                    digit.set(i+1, digit.get(i+1)+1);
+                }
+            }
+        }
+    }
+    
+    public void print(){
+        for(int i=length-1;i>=0;i--){
+            System.out.print(digit.get(i)+",");
+        }
+        System.out.println("");
+    }
+    
+    public static void main(String[] args) {
+        Base26 num = new Base26();
+        
+        for(int i=0;i<256;i++){
+            num.increment();
+            num.print();
+        }      
+    }
+    
+    public int[] toIntArray(){
+        int[] array = new int[length];
+        
+        int i=0;
+        int j=length-1;
+        
+        while(i < length){
+            array[i] = digit.get(j)-1;
+            i++;
+            j--;
+        }
+        
+        return array;
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# ALGORITHMS MIDTERM PROJECT
+
+Project for Midterms
+## IDE
+
+Netbeans 8.2
